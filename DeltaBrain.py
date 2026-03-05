3a35caa94c7133aaf4ace5c821108151b6fd43c8
Rempest <thehuman130410@gmail.com>
2026-03-05 20:13:31 +0500

    Refactor DeltaBrain to use DeltaAnalyze

--- DeltaBrain.py
+++ DeltaBrain.py
@@ -1,10 +1,20 @@
-from DeltaACT as DeltaACT
+from DeltaAnalyze as DeltaACT
 from DeltaTensors as DeltaSensors
 class DeltaBrain(self):
   def __init__(self):
-    self.brain = DeltaBrain()
-    self.tensor = DeltaSensors()
+    self.brain = DeltaACT()
+    self.sensor = DeltaSensors()
   def run(self)
-    print("analyze a photo..")
-    vision_ok.DeltaACT()
-    
+    print("Analyzing  photo..")
+    vision_ok = self.brain.DeltaACT()
+    if not vision_ok:
+      return False
+  print("checking sensors..")
+  result = self.sensor.DeltaSensors()
+    if result == "STOP":
+      print("Robot is stoped")
+    else
+      print("Robot is working", result)
+robot = DeltaBrain()
+robot.run()
+
