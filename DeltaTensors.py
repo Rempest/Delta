e7c70bcaac26f7ddfcdf0eaa3956658e44d99f4a
Rempest <thehuman130410@gmail.com>
2026-03-05 19:39:40 +0500

    Fixing bug and improve the code
    On this time, i to correct  my bugs and improve a sensor work

--- DeltaTensors.py
+++ DeltaTensors.py
@@ -1,12 +1,18 @@
 import numpy as np
 class Delta_arrays:
   def DeltaTen(self):
-    sensor_data = np.array(2.5, 2.9, 3.0, 1.9, 2.7)
+    #data of the sensors
+    sensor_data = np.array([2.5, 2.9, 3.0, 1.9, 2.7])
     min_sensor_data = np.min(sensor_data)
-    if np.any(sensor_data = 1):
+    #the safe
+    if (min_sensor_data <= 1.0):
       print("STOP!")
+      return
     best_sensor_data = np.argmax(sensor_data)
     way = ["Left", "Left-front", "Straight", "Right-front", "Right"]
     print("The best way it is:", way[best_sensor_data])
+    #starting
+delta = Delta_arrays()
+delta.DeltaTen()
     
     
