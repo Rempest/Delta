c30bffa7214e49197a32223ff9e459cf4180047d
Rempest <thehuman130410@gmail.com>
2026-03-05 19:57:27 +0500

    Rename Delta_arrays to DeltaSensors and fix conditions
    Refactor Delta_arrays to DeltaSensors and update logic.

--- DeltaTensors.py
+++ DeltaTensors.py
@@ -1,16 +1,18 @@
 import numpy as np
-class Delta_arrays:
+import cv2
+class DeltaSensors:
   def DeltaTen(self):
     #data of the sensors
     sensor_data = np.array([2.5, 2.9, 3.0, 1.9, 2.7])
     min_sensor_data = np.min(sensor_data)
     #the safe
-    if (min_sensor_data <= 1.0):
+    if min_sensor_data <= 1.0:
       print("STOP!")
-      return
+      return False
     best_sensor_data = np.argmax(sensor_data)
     way = ["Left", "Left-front", "Straight", "Right-front", "Right"]
     print("The best way it is:", way[best_sensor_data])
+    return way[best_index]
     #starting
 delta = Delta_arrays()
 delta.DeltaTen()
