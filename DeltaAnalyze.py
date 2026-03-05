bf14ce5b1b7dcf7a284246598802ffa4b1535197
Rempest <thehuman130410@gmail.com>
2026-03-05 20:26:17 +0500

    Correct typos in DeltaAnalyze.py
    Fix typos in variable names and print statements.

--- DeltaAnalyze.py
+++ DeltaAnalyze.py
@@ -8,21 +8,18 @@ class DeltaED:
        print("Image not found!")
        return False
       #edit a photo
-     self.odject_resiz = cv2.resize(self.object, (500, 500))
+     self.odject_resize = cv2.resize(self.object, (500, 500))
      self.object_gray =  cv2.cvtColor(self.object_resize, cv2.COLOR_BGR2GRAY)
      self.odject_blur =cv2.GaussianBlur(self.object_gray, (5, 5), 0)
      self.object_canny = cv2.Canny(self.object_blur, 100, 300)
      return True
      ##imshow a photo
    def DeltaShow(self):
-    cv2.imshow('Original onject: ', self.object_resize)
+    cv2.imshow('Original object: ', self.object_resize)
     cv2.imshow('Gray style object: ', self.object_gray)
     cv2.imshow('Canny: ', self.object_canny) 
     cv2.imshow('Blur: ', self.object_blur)
     print("Enter the any symbol on the keyboard to close window..")
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-     #main
-Delta_object = DeltaPrime()
-Delta_object.DeltaACT()
-DeltaACT.DeltaShow()
+
