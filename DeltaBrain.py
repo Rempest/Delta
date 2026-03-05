6e99e57ed2ae09b3a3d8ce468ea485da0dc7ea81
Rempest <thehuman130410@gmail.com>
2026-03-05 20:02:15 +0500

    Add DeltaBrain class with initialization and run method

--- DeltaBrain.py
+++ DeltaBrain.py
@@ -1,1 +1,10 @@
-
+from DeltaACT as DeltaACT
+from DeltaTensors as DeltaSensors
+class DeltaBrain(self):
+  def __init__(self):
+    self.brain = DeltaBrain()
+    self.tensor = DeltaSensors()
+  def run(self)
+    print("analyze a photo..")
+    vision_ok.DeltaACT()
+    
