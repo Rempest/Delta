4d1b1eaf6e83960943c0a3465c5df2ecd7e14038
Rempest <thehuman130410@gmail.com>
2026-04-09 14:30:47 +0500

    Fix import statements and method definitions

--- DeltaBrain.py
+++ DeltaBrain.py
@@ -1,17 +1,17 @@
-from DeltaAnalyze as DeltaED
-from DeltaTensors as DeltaSensors
-class DeltaBrain(self):
+from DeltaAnalyze import DeltaED
+from DeltaTensors import DeltaSensors
+class DeltaBrain:
   def __init__(self):
     self.brain = DeltaED()
     self.sensor = DeltaSensors()
-  def run(self)
+  def run(self):
      print("Analyzing  photo..")
     vision_ok = self.brain.DeltaACT()
     if not vision_ok:
       print("The odject is failed...")
       return False
       print("checking sensors..")
-     result = self.sensor.DeltaSensors()
+     result = self.sensor.DeltaTen()
     if result == "STOP":
       print("Robot is stoped")
     else:
