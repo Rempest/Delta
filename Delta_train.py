a9cf0a3e485e0fa50f1b72c70f6acca4f2939cf0
Raempest <thehuman130410@gmail.com>
2026-04-20 22:13:43 +0500

    Fix YOLO model name and correct epochs parameter

--- Delta_train.py
+++ Delta_train.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
-DeltaPrime = YOLO("yolovn.pt")
-Deltaprime.train(data = "VisDrone.yaml", epoch = 70)
+DeltaPrime = YOLO("yolov8n.pt")
+Deltaprime.train(data = "VisDrone.yaml", epochs = 70)
