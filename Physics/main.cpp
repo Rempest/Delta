3ae541077ef4f43deeed50e2f5315f2e1a2d33a8
Raempest <thehuman130410@gmail.com>
2026-05-09 13:52:44 +0500

    Update main.cpp

--- Physics/main.cpp
+++ Physics/main.cpp
@@ -1,44 +1,47 @@
 #include <iostream>
+#include <iomanip>
 #include "State.h"
 #include "Dynamics.h"
 #include "PID.h"
 #include "Planner.h"
-#include "Sensors.h"
-
-using namespace std;
+#include "Distance.h"
 
 int main() {
+    try {
+        State state;
+        Dynamics dynamics(1.0, 0.1);
+        PID pid(2.0, 0.0, 0.5);
+        Planner planner(10.0);
+        Sensors sensors(1.0);
 
-    State state;
-    Dynamics dynamics;
-    PID pid(2.0, 0.0, 0.5);
-    Planner planner;
-    Sensors sensors;
+        const double dt = 0.1;
+        const int steps = 100;
 
-    double dt = 0.1;
+        std::cout << std::fixed << std::setprecision(3);
 
-    for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < steps; i++) {
+            double target_velocity;
 
-        double target_velocity;
+            if (sensors.isObstacleClose()) {
+                target_velocity = 0;
+                std::cout << "STOP\n";
+            } else {
+                target_velocity = planner.computeTargetVelocity(state);
+            }
 
-        if (sensors.isObstacleClose()) {
-            target_velocity = 0;
-            cout << "STOP\n";
-        } else {
-            target_velocity = planner.computeTargetVelocity(state);
+            double thrust = pid.update(target_velocity, state.v, dt);
+            state.a = dynamics.computeAcceleration(thrust, state.v);
+            state.v += state.a * dt;
+            state.x += state.v * dt;
+
+            std::cout << "x: " << state.x
+                      << " v: " << state.v
+                      << " a: " << state.a << "\n";
         }
 
-        double thrust = pid.update(target_velocity, state.v, dt);
-
-        state.a = dynamics.computeAcceleration(thrust, state.v);
-
-        state.v += state.a * dt;
-        state.x += state.v * dt;
-
-        cout << "x: " << state.x
-             << " v: " << state.v
-             << " a: " << state.a
-             << endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
     }
 
     return 0;
