fd79a97d7fbb67abbda2f17480f65cc73a134b75
Raempest <thehuman130410@gmail.com>
2026-05-09 13:46:03 +0500

    Update Distance.h

--- Physics/Distance.h
+++ Physics/Distance.h
@@ -2,9 +2,17 @@
 
 class Sensors {
 public:
-    double distance = 5.0;
+    explicit Sensors(double threshold = 1.0)
+        : threshold_(threshold), distance_(5.0) {}
 
-    bool isObstacleClose() {
-        return distance < 1.0;
+    void setDistance(double d) { distance_ = d; }
+    double getDistance() const { return distance_; }
+
+    bool isObstacleClose() const {
+        return distance_ < threshold_;
     }
+
+private:
+    double distance_;
+    double threshold_;
 };
