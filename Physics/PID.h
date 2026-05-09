7abd3d3b25b52c2e6a2683ef62cc260a6cc3d02f
Raempest <thehuman130410@gmail.com>
2026-05-09 13:47:01 +0500

    Update PID.h

--- Physics/PID.h
+++ Physics/PID.h
@@ -1,23 +1,38 @@
 #pragma once
+#include <stdexcept>
+#include <algorithm>
 
 class PID {
-private:
-    double kp, ki, kd;
-    double prev_error = 0;
-    double integral = 0;
-
 public:
-    PID(double p, double i, double d)
-        : kp(p), ki(i), kd(d) {}
+    PID(double kp, double ki, double kd, double max_integral = 10.0)
+        : kp_(kp), ki_(ki), kd_(kd),
+          max_integral_(max_integral),
+          prev_error_(0), integral_(0) {}
 
     double update(double target, double current, double dt) {
+        if (dt <= 0)
+            throw std::invalid_argument("dt must be positive");
+
         double error = target - current;
 
-        integral += error * dt;
-        double derivative = (error - prev_error) / dt;
+        integral_ += error * dt;
+        // Anti-windup: ограничиваем интеграл
+        integral_ = std::clamp(integral_, -max_integral_, max_integral_);
 
-        prev_error = error;
+        double derivative = (error - prev_error_) / dt;
+        prev_error_ = error;
 
-        return kp * error + ki * integral + kd * derivative;
+        return kp_ * error + ki_ * integral_ + kd_ * derivative;
     }
+
+    void reset() {
+        prev_error_ = 0;
+        integral_ = 0;
+    }
+
+private:
+    double kp_, ki_, kd_;
+    double max_integral_;
+    double prev_error_;
+    double integral_;
 };
